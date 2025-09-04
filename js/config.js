fc75dad79c616c872c8a3621a5dfdffbae5d5cb6
max-az-10 <maximelegenie@gmail.com>
2025-09-04 11:36:51 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_43a7oDxis', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'tmet9jd9bf6512kqkoou9sgmo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_EnGRIZQJp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ckfrscfvjk708rq75p7n7f269', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
